a20c1c751a6e03805f758a5b002835c395821560
Rafaela Bárbara Custódio <rafa.b.custodio13@gmail.com>
2016-03-01 21:02:48 -0300

    Create Exponencial.java

---
+++ exercicio2/Exponencial.java
@@ -0,0 +1,11 @@
+public class Exponencial {
+    public static void main(String[] args) {
+      int i,q,c;
+      System.out.println("#" + "  " + "Q" + "  " + "C");  
+      for(i=0;i<11;i++){
+        q=i*i;
+        c=i*i*i;
+        System.out.println(i + "  " + q + "  "+ c);
+      }
+  }
+}
