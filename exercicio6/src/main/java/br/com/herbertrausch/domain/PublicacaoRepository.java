581b6561d62858812c0066a142728ee2c3e52712
Rafaela1301 <rafa.b.custodio13@gmail.com>
2016-04-14 10:14:37 -0300

    lp2

---
+++ exercicio6/src/main/java/br/com/herbertrausch/domain/PublicacaoRepository.java
@@ -0,0 +1,14 @@
+package br.com.herbertrausch.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublicacaoRepository  extends CrudRepository<Publicacao, Long> {
+	
+List<Publicacao> findByPagina(String pagina);
+List<Publicacao> findByTexto(String texto);
+
+} 
+
+
---
+++ exercicio6/src/main/java/br/com/herbertrausch/domain/UsuarioRepository.java
@@ -0,0 +1,14 @@
+package br.com.herbertrausch.domain;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface UsuarioRepository extends CrudRepository<Usuario, Long>{
+	
+	List<Usuario> findByNome(String nome);
+	List<Usuario> findBySenha(String senha);
+	List<Usuario> findById(String id);
+	
+}
